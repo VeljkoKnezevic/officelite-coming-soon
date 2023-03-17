59485a3907dd7d3fd4719e43517193f85c9ca9c6
Veljko <veljkoBitno@gmail.com>
2023-03-17 11:52:58 +0100

    Countdown timer working

--- src/components/DateComponent.jsx
+++ src/components/DateComponent.jsx
@@ -1,34 +1,43 @@
+/* eslint-disable react/prop-types */
 import { Link, useLocation } from "react-router-dom";
-import { format, addDays } from "date-fns";
+import { format, addDays, addHours } from "date-fns";
+import Countdown from "react-countdown";
 
 export default function DateComponent() {
   const location = useLocation();
 
-  const futureDate = addDays(new Date(), 30);
+  const futureDate = addHours(addDays(new Date(), 30), 1);
   const date = format(futureDate, "d MMM yyyy");
 
   return (
     <section className={location.pathname === "/sign-up" ? "signup__date" : "date"}>
-      {/* If you're choosing to make this date dynamic, set it to 30 days in the future from the moment the visitor views the page */}
       <h3 className={location.pathname === "/sign-up" ? "signup__date__date" : "date__date"}>
         Coming <span className="date__date--blue">{date}</span>
       </h3>
-      {/* If you're choosing to make this timer dynamic, have it countdown from the date you set above */}
       <div className={location.pathname === "/sign-up" ? "signup__date__time" : "date__time"}>
         <div className="date__card">
-          {/* <div className="date__card__number">{}</div> */}
+          <div className="date__card__number">
+            <Countdown date={futureDate} renderer={(props) => <div>{props.days}</div>} />
+          </div>
           <div className="date__card__unit">Days</div>
         </div>
         <div className="date__card">
-          {/* <div className="date__card__number">{}</div> */}
+          <div className="date__card__number">
+            <Countdown date={futureDate} renderer={(props) => <div>{props.hours}</div>} />
+          </div>
           <div className="date__card__unit">Hours</div>
         </div>
         <div className="date__card">
-          {/* <div className="date__card__number">{}</div> */}
+          <div className="date__card__number">
+            <Countdown date={futureDate} renderer={(props) => <div>{props.minutes}</div>} />
+          </div>
           <div className="date__card__unit">Min</div>
         </div>
         <div className="date__card">
-          {/* <div className="date__card__number">{}</div> */}
+          <div className="date__card__number">
+            <Countdown date={futureDate} renderer={(props) => <div>{props.seconds}</div>} />
+          </div>
+
           <div className="date__card__unit">Sec</div>
         </div>
       </div>
